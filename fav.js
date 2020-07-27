a745819a5dd6dbb5888f9245897ab39458d90154
Jejak Unik <58129525+jejakunikblog@users.noreply.github.com>
2020-07-27 18:08:36 +0700

    Add files via upload

---
+++ fav.js
@@ -0,0 +1,38 @@
+{
+  "name": "Jejak Unik",
+  "short_name": "Jejak Unik",
+  "start_url": "https://www.jejakunik.com/",
+  "display": "standalone",
+  "background_color": "#fff",
+  "theme_color": "#C51162",
+  "description": "Blog Seputar Dunia Traveling, Kuliner Dan Hotel Terupdate.",
+  "icons": [{
+    "src": "https://1.bp.blogspot.com/-e5TnoDa5dtg/Xx601Wrf1JI/AAAAAAAABF8/rn38vVhNULEIlexDu4SkTxJyc7ZTPoqGgCLcBGAsYHQ/s200/ju-2%2B%25281%2529.png",
+    "sizes": "48x48",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-e5TnoDa5dtg/Xx601Wrf1JI/AAAAAAAABF8/rn38vVhNULEIlexDu4SkTxJyc7ZTPoqGgCLcBGAsYHQ/s200/ju-2%2B%25281%2529.png",
+    "sizes": "72x72",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-e5TnoDa5dtg/Xx601Wrf1JI/AAAAAAAABF8/rn38vVhNULEIlexDu4SkTxJyc7ZTPoqGgCLcBGAsYHQ/s200/ju-2%2B%25281%2529.png",
+    "sizes": "96x96",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-e5TnoDa5dtg/Xx601Wrf1JI/AAAAAAAABF8/rn38vVhNULEIlexDu4SkTxJyc7ZTPoqGgCLcBGAsYHQ/s200/ju-2%2B%25281%2529.png",
+    "sizes": "144x144",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-e5TnoDa5dtg/Xx601Wrf1JI/AAAAAAAABF8/rn38vVhNULEIlexDu4SkTxJyc7ZTPoqGgCLcBGAsYHQ/s200/ju-2%2B%25281%2529.png",
+    "sizes": "168x168",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-e5TnoDa5dtg/Xx601Wrf1JI/AAAAAAAABF8/rn38vVhNULEIlexDu4SkTxJyc7ZTPoqGgCLcBGAsYHQ/s200/ju-2%2B%25281%2529.png",
+    "sizes": "192x192",
+    "type": "image/png"
+  }, {
+    "src": "https://1.bp.blogspot.com/-e5TnoDa5dtg/Xx601Wrf1JI/AAAAAAAABF8/rn38vVhNULEIlexDu4SkTxJyc7ZTPoqGgCLcBGAsYHQ/s200/ju-2%2B%25281%2529.png",
+    "sizes": "512x512",
+    "type": "image/png"
+  }]
+}
